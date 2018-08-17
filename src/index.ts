0c3094e44f440b9ee286f03467ce10a294723cdd
stan e <yi.hang@live.com>
2018-08-17 15:21:48 +0800

    增加 delon form组件

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ export * from './abp/index';
 export * from './abc/index';
 // export * from './auth/index';
 export * from './cache/index';
-// export * from './form/index';
+export * from './form/index';
 export * from './theme/index';
 export * from './util/index';
