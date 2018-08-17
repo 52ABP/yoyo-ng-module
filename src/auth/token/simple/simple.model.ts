7ff1e60387842da5549b93fd2775e649ff9d86f1
stan e <yi.hang@live.com>
2018-08-17 16:22:33 +0800

    增加delon auth代码

---
+++ src/auth/token/simple/simple.model.ts
@@ -0,0 +1,7 @@
+import { ITokenModel } from '../interface';
+
+export class SimpleTokenModel implements ITokenModel {
+  [key: string]: any;
+
+  token: string;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from './abp/index';
 export * from './abc/index';
-// export * from './auth/index';
+export * from './auth/index';
 export * from './cache/index';
 export * from './form/index';
 export * from './theme/index';
