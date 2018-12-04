c515aed9e6e560ec4ddb24f9719976468d7589c7
stan e <yi.hang@live.com>
2018-12-04 19:14:25 +0800

    暂时移除对 form 表单的导出

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export * from './abp/index';
 export * from './abc/index';
 // export * from './auth/index';
 export * from './cache/index';
-export * from './form/index';
+// export * from './form/index';
 export * from './theme/index';
 export * from './chart/index';
 export * from './mock/index';
